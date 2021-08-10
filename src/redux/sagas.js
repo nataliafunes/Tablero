388e0d726df1ae65815a791b6f4130308dcec504
nataliafunes <natalia_funes@hotmail.com>
2021-08-10 16:06:37 -0300

    Updates

--- src/redux/sagas.js
+++
@@ -1,8 +0,0 @@
-import { all } from 'redux-saga/effects';
-import admTareasReducerSagas from './Adm-tareas/saga.ts';
-
-export default function* rootSaga() {
-  yield all([
-   admTareasReducerSagas()
-  ]);
-}
